aaa4b69cb5e4fb1b7e0de734d3baff8c8509ae69
korneliusz <korneliusz.caputa@siili.fi>
2015-08-22 10:24:06 +0200

    Add the rest of exception examples.

--- lyahfgg/9_5_exceptions.hs
+++ lyahfgg/9_5_exceptions.hs
@@ -9,8 +9,16 @@
 
 import System.Environment
 import System.Directory
+import System.IO
+import System.IO.Error
+
 
 main = do
+    withoutExceptions
+    -- withExceptions
+
+withoutExceptions :: IO ()
+withoutExceptions = do
     (fileName:_) <- getArgs
     fileExists <- doesFileExist fileName
     if fileExists
@@ -22,3 +30,20 @@ main = do
                 " lines."
         else do
             putStrLn "The file does not exist."
+
+-- The packages seem to have changed in newer versions.
+-- withExceptions :: IO ()
+-- withExceptions = toTry `catch` errorHandler
+
+-- toTry :: IO ()
+-- toTry =do
+--   (fileName:_) <- getArgs
+--   contents <- readFile fileName
+--   putStrLn $
+--       "The file has " ++
+--       show (length (lines contents)) ++
+--       " lines."
+-- errorHandler :: IOError -> IO ()
+-- errorHandler e
+--     | isDoesNotExistError e = putStrLn "The file doesn't exist!"
+--     | otherwise = ioError e
