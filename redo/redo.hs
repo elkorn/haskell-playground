c04ef96074d935e6608662c7fbf24f2ef6460801
Korneliusz Caputa <helluinster@gmail.com>
2015-10-12 20:10:21 +0200

    Add new features.
    - directory support
    - phony target support
    - use MD5 as file names and paths as dependency contents

--- redo/redo.hs
+++ redo/redo.hs
@@ -3,20 +3,22 @@
 import Control.Exception (catch, catchJust, IOException) -- SomeException being the root exception type.
 import Control.Monad (filterM, liftM, unless, guard)
 import qualified Data.ByteString.Lazy as BL
-import Data.Digest.Pure.MD5
+import qualified Data.Digest.Pure.MD5 as MD5
 import Data.Map.Lazy (fromList, insert, toList, adjust)
 import Data.Maybe (listToMaybe)
 import Debug.Trace (traceShow)
 import GHC.IO.Exception (IOErrorType(..))
 import System.Directory
        (renameFile, removeFile, doesFileExist, getDirectoryContents,
-        removeDirectoryRecursive, createDirectoryIfMissing)
+        removeDirectoryRecursive, createDirectoryIfMissing,
+        getCurrentDirectory, setCurrentDirectory)
 import System.Exit (ExitCode(..))
 import System.FilePath
-       (replaceBaseName, hasExtension, takeBaseName, (</>))
+       (replaceBaseName, hasExtension, takeBaseName, (</>), splitFileName)
 import System.Environment
        (getArgs, getEnvironment, getProgName, lookupEnv)
-import System.IO (hPutStrLn, stderr)
+import System.IO
+       (hPutStrLn, stderr, IOMode(..), withFile, hFileSize)
 import System.IO.Error (ioeGetErrorType, isDoesNotExistError)
 import System.Process
        (createProcess, CreateProcess(..), waitForProcess, shell)
@@ -34,7 +36,7 @@ metaDir = ".redo"
 main :: IO ()
 -- main = getArgs >>= mapM_ redo
 main =
-  do mapM_ redo =<< getArgs
+  do mapM_ redoWithDirectorySupport =<< getArgs
      progName <- getProgName
      possibleRedoTarget <- lookupEnv redoTargetKey
      case (progName,possibleRedoTarget) of
@@ -44,27 +46,33 @@ main =
        ("redo-ifchange",Nothing) ->
          error "Missing REDO_TARGET environment variable."
        _ -> return ()
-  where writeMD5 redoTarget dep =
-          writeFile (metaDir </> redoTarget </> dep) =<<
-          md5' dep
+  where redoWithDirectorySupport :: String -> IO ()
+        redoWithDirectorySupport target =
+          do topDir <- getCurrentDirectory
+             let (dir,file) = splitFileName target
+             setCurrentDirectory dir
+             redo file
+             setCurrentDirectory topDir
 
 redo :: String -> IO ()
 redo target =
-  do upToDate' <-
-       traceShow' `liftM`
-       (upToDate metaDepsDir)
+  do upToDate' <- (upToDate target)
      unless upToDate' $
        maybe missingDo redo' =<<
-       redoPath target
+       doPath target
   where redo' :: FilePath -> IO ()
         redo' path =
           do recreateDirectory metaDepsDir
-             (writeFile (metaDepsDir </> path)) =<<
-               md5' path
+             writeMD5 target path
+             -- (writeFile (metaDepsDir </> path)) =<<
+             --   fileMd5 path
              exit <- runShell $ redoCommand path
              case exit of
                ExitSuccess ->
-                 do renameFile tmp target
+                 do size <- fileSize tmp
+                    if size > 0
+                       then renameFile tmp target
+                       else removeFile tmp -- phony target handling
                ExitFailure code ->
                  do removeFile tmp
                     hPutStrLn stderr $ "Redo script exited with non-zero exit code: " ++
@@ -77,7 +85,7 @@ redo target =
                "'"
         redoCommand :: String -> String
         redoCommand path =
-          unwords ["sh",path,"0",takeBaseName target,tmp,">",tmp]
+          unwords ["sh -x",path,"0",takeBaseName target,tmp,">",tmp]
         runShell :: String -> IO ExitCode
         runShell cmd =
           do oldEnv <- getEnvironment
@@ -99,8 +107,8 @@ redo target =
         tmp = target ++ "---redoing"
         metaDepsDir = metaDir </> target
 
-redoPath :: FilePath -> IO (Maybe FilePath)
-redoPath target =
+doPath :: FilePath -> IO (Maybe FilePath)
+doPath target =
   listToMaybe `liftM`
   filterM doesFileExist candidates
   where candidates =
@@ -111,22 +119,32 @@ redoPath target =
              else []
 
 upToDate :: FilePath -> IO Bool
-upToDate metaDepsDir =
-  catch (do dependencies <- getDirectoryContents metaDepsDir
-            (all id) `liftM`
-              mapM depUpToDate dependencies)
+upToDate target =
+  catch (do exists <- doesFileExist target
+            if exists
+               then do md5s <- getDirectoryContents $ metaDir </> target
+                       and `liftM`
+                         mapM depUpToDate md5s
+               else return False)
         (\(_ :: IOException) -> return False)
   where
         -- return $ all $ mapM $ depUpToDate dependencies
-        depUpToDate :: FilePath -> IO Bool
-        depUpToDate dep =
-          catch (do let path = metaDepsDir </> dep
-                    oldMd5 <-
-                      (head . words) -- getting the first entry in the MD5 because of the 'md5 filename' format that `md5sum` provides.
-                       `liftM`
-                      readFile path
-                    newMd5 <- md5' dep
-                    return $ oldMd5 == newMd5)
+        depUpToDate :: String -> IO Bool
+        depUpToDate oldMd5 =
+          catch (do let path = metaDir </> target </> oldMd5
+                    dep <-
+                           -- (head . words) -- getting the first entry in the MD5 because of the 'md5 filename' format that `md5sum` provides.
+                           --  `liftM`
+                           readFile path
+                    newMd5 <- fileMd5 dep
+                    doScript <- doPath dep
+                    case doScript of
+                      Nothing -> return $ oldMd5 == newMd5
+                      Just _ ->
+                        do upToDate' <- upToDate dep
+                           return $
+                             (oldMd5 == newMd5) &&
+                             upToDate')
                 (\e -> return $ ioeGetErrorType e == InappropriateType -- (\e -> return if ioeGetErrorType e == InappropriateType)
                                                                       -- then True -- Treat it as a correct dependency, even though it is not a dependency. It's a way of ignoring '.' and '..' directories.
                                                                       -- else False)
@@ -139,7 +157,16 @@ recreateDirectory path =
                (\_ -> return ())
      createDirectoryIfMissing True path
 
-md5' :: FilePath -> IO String
-md5' path =
-  (show . md5) `liftM`
+fileMd5 :: FilePath -> IO String
+
+
+fileMd5 path =
+  (show . MD5.md5) `liftM`
   BL.readFile path
+writeMD5 :: String -> FilePath -> IO ()
+writeMD5 redoTarget dep =
+  do md5 <- fileMd5 dep
+     writeFile (metaDir </> redoTarget </> md5) dep
+
+fileSize :: FilePath -> IO Integer
+fileSize path = withFile path ReadMode hFileSize
