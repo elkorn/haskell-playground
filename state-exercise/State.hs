eee171a0748a07870d80fa9993aab8ea6fc01eaf
Korneliusz Caputa <helluinster@gmail.com>
2015-10-14 21:01:44 +0200

    Indent fix in State.

--- state-exercise/State.hs
+++ state-exercise/State.hs
@@ -16,17 +16,15 @@ modify fn = State (\s -> ((),fn s))
 instance Functor (State s) where
   -- fmap :: (a -> b) -> (State s) a -> (State s) b
   fmap fn (State st) =
-    State (\s ->
-               let (a,s2) = st s
-               in (fn a,s2))
+    State (\s -> let (a,s2) = st s
+                 in (fn a,s2))
 
 instance Applicative (State s) where
   -- pure :: a -> (State s) a
   pure = undefined
   -- (<*>) :: (State s) (a -> b) -> (State s) a -> (State s) b
   -- How should the sequencing look like?
-  (State fnSt) <*> (State st) = State (\s -> let
-                                                 (fn, s2) = fnSt s
+  (State fnSt) <*> (State st) = State (\s -> let (fn, s2) = fnSt s
                                                  (a,s3) = st s2
                                             in (fn a, s3))
 
