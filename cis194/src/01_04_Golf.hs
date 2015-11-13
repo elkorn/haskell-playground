b460eb499a919cfeb1b0882f72aca75e7ab8a926
Korneliusz Caputa <helluinster@gmail.com>
2015-11-13 18:11:34 +0100

    Add histogram exercise.

--- cis194/src/01_04_Golf.hs
+++ cis194/src/01_04_Golf.hs
@@ -28,3 +28,32 @@ localMaxima (x:y:z:zs)
   | otherwise = rest
   where rest = (localMaxima $ y : z : zs)
 localMaxima _ = []
+
+histogram :: [Int] -> String
+histogram xs = hBody ++ hTail
+  where numRange = [0 .. 9]
+        hHeight :: Int
+        hHeight = maximum xs
+        hBody :: String
+        hBody =
+          concat $
+          map hRow $
+          reverse [0 .. hHeight]
+        hRow :: Int -> String
+        hRow lvl =
+          concat $
+          (map (\n ->
+                  if ((length $
+                       elemIndices n xs) >
+                      lvl)
+                     then "*"
+                     else " ")
+               numRange) ++
+          ["\n"]
+        hTail :: String
+        hTail =
+          concat $
+          (map (\_ -> "=") numRange) ++
+          ["\n"] ++
+          (map show numRange) ++
+          ["\n"]
