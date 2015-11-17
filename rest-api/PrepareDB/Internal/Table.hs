d3c298ba3a47be2c7d3365fed5d264ae3f594956
Korneliusz Caputa <helluinster@gmail.com>
2015-11-17 20:19:53 +0100

    Fix table creation.

--- rest-api/PrepareDB/Internal/Table.hs
+++ rest-api/PrepareDB/Internal/Table.hs
@@ -1,4 +1,4 @@
-module Internal.Table where
+module Internal.Table (Table(..), create, createIfNotExists) where
 
 import Internal.Column (Column, statement)
 
@@ -6,5 +6,20 @@ data Table =
   Table String
         [Column]
 
+
+prettyJoin :: (Show a) => [a] -> String
+prettyJoin (x:[]) = show x
+prettyJoin (x:xs) = show x ++ ",\n" ++ prettyJoin xs
+prettyJoin [] = ""
+
+doCreate :: Maybe String -> Table -> String
+doCreate (Just precondition) (Table tableName columns) = unwords [
+  "CREATE TABLE " ,
+  precondition , " " , tableName , "(\n" , (prettyJoin $ map statement columns) , ");"]
+doCreate Nothing table = doCreate (Just "") table
+
 create :: Table -> String
-create (Table tableName columns) = "CREATE TABLE " ++ tableName ++ "(\n" ++ (unlines $ map (\col -> (statement col )++ ",") columns) ++ ");"
+create = doCreate Nothing
+
+createIfNotExists :: Table -> String
+createIfNotExists = doCreate (Just "IF NOT EXISTS")
--- rest-api/PrepareDB/Main.hs
+++ rest-api/PrepareDB/Main.hs
@@ -1,17 +1,25 @@
 module Main where
 
-import Database.HDBC.Sqlite3 (connectSqlite3)
+import Database.HDBC.Sqlite3 (connectSqlite3, Connection)
 import Database.HDBC
 
 import Internal.Table
 
 import Tables.Task (task)
 
+tables :: [Table]
+tables = [task]
+
+createTable :: Connection -> Table -> IO Integer
+createTable conn table = run conn (createIfNotExists table) []
+
 createAll :: String -> IO ()
 createAll dbName = do
-  -- conn <- connectSqlite3 dbName
-  putStrLn $ create task
-
+  conn <- connectSqlite3 dbName
+  createResult <- mapM (createTable conn) tables
+  commit conn
+  disconnect conn
+  putStrLn $ show createResult
 
 main :: IO ()
 main = createAll "restApi.db"
