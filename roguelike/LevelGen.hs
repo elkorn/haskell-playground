bfc2a15bec028afcb13551ffd550d0ad4c9c2bac
korneliusz <korneliusz.caputa@siili.fi>
2016-01-05 20:41:51 +0100

    Add buggy corridor melding.

--- roguelike/LevelGen.hs
+++ roguelike/LevelGen.hs
@@ -31,6 +31,7 @@ data Room
            , roomConnections :: S.Set Room
            }
     | Corridor { corridorCoordinates :: [RectBoundaries]
+               , corridorGoesThroughRooms :: S.Set Room
                }
     deriving (Ord,Eq,Show)
 
@@ -47,12 +48,11 @@ generateLevel spec = do
             }
     rooms <- generateRooms baseLevel [] 2
     corridors <- generateCorridors rooms
-    print corridors
     return $
         updateLevelMax $
         baseLevel
         { levelTiles = foldl M.union M.empty $
-          map roomToTiles rooms
+          map roomToTiles (rooms ++ corridors) 
         }
 
 roomToTiles :: Room -> M.Map Coordinates Tile
@@ -71,6 +71,19 @@ roomToTiles (Room bounds@((startX, startY), (endX, endY)) meldedRooms _) =
     meldBounds = map roomCoordinates $ S.toList meldedRooms
     isBoundary :: Int -> Int -> Int -> Bool
     isBoundary coord startBound endBound = coord == startBound || coord == endBound
+roomToTiles (Corridor legs crossedRooms) = let
+  legRoomsWithoutIntersections = map legToRoom legs--foldl M.union M.empty (map (roomToTiles . legToRoom))
+  legRoomsWithIntersections =
+    map updateCrossIntersections legRoomsWithoutIntersections
+  legToRoom :: RectBoundaries -> Room
+  legToRoom leg = Room leg crossedRooms S.empty
+  updateCrossIntersections :: Room -> Room
+  updateCrossIntersections room =
+    room {roomMeldedWith = S.union (roomMeldedWith room) $ S.fromList $ except room legRoomsWithoutIntersections }
+  in foldl M.union M.empty $ map roomToTiles legRoomsWithIntersections
+
+except :: (Eq a) => a -> [a] -> [a]
+except val = filter (/=val)
 
 generateRooms :: Level -> [Room] -> Int -> IO [Room]
 generateRooms level existingRooms 0 = return existingRooms
@@ -86,7 +99,9 @@ generateRooms level existingRooms n = do
         let newRoomIntersectsExistingOnes = length intersectingRooms > 0
         if newRoomIntersectsExistingOnes
         then do
-             shouldMeld <- coinFlip
+             -- shouldMeld <- coinFlip
+             shouldMeld <- return False
+
              if shouldMeld
                 then return (meldRooms room existingRooms intersectingRooms, n-1)
                 else return (existingRooms, n)
@@ -111,6 +126,10 @@ intersects ((x1a,y1a),(x2a,y2a)) ((x1b,y1b),(x2b,y2b)) =
 roomIntersects :: Room -> Room -> Bool
 roomIntersects (Room boundsA _ _) (Room boundsB _ _) =
   intersects boundsA boundsB
+roomIntersects (Corridor legs _) (Room roomBounds _ _) =
+  any (intersects roomBounds) legs
+roomIntersects room@(Room _ _ _) corridor@(Corridor _ _) =
+  roomIntersects corridor room
 
 intersection :: RectBoundaries -> RectBoundaries -> RectBoundaries
 intersection ((x1a,y1a),(x2a,y2a)) roomB =
@@ -144,7 +163,7 @@ meldRooms roomToMeldIn roomsToMeldInto intersectingRooms =
 generateCorridors :: [Room] -> IO [Room]
 generateCorridors rooms = do
   connections <- generateConnections rooms
-  return $ createCorridorsForConnections connections
+  return $ map updateCrossedRooms $ createCorridorsForConnections connections
   where
     generateConnections :: [Room] -> IO [(Room, Room)]
     generateConnections rooms = do
@@ -156,7 +175,6 @@ generateCorridors rooms = do
     selectRandom howMany items = do
         stdGen <- getStdGen
         let indices = randomRs (0, (length items) - 1) stdGen
-        print (take howMany indices)
         return $ map (items !!) (take howMany indices)
     createCorridorsForConnections :: [(Room, Room)] -> [Room]
     createCorridorsForConnections connections = foldl (\result connection -> (createCorridor connection):result) [] connections
@@ -164,7 +182,7 @@ generateCorridors rooms = do
     createCorridor (Room ((x1a, y1a), (x2a, y2a)) _ _, Room ((x1b, y1b), (x2b, y2b)) _ _) = let
       (startX, endX) = getInner (x1a, x2a) (x1b, x2b)
       (startY, endY) = getInner (y1a, y2a) (y1b, y2b)
-      in Corridor [((startX, startY), (endX, startY + 2)),((startX, startY), (startX + 2, endY))]
+      in Corridor [((startX, startY), (endX, startY + 2)),((startX, startY), (startX + 2, endY))] S.empty
     getInner :: (Int, Int) -> (Int, Int) -> (Int, Int) -- These are not correct coordinates, the format is (x,x) or (y,y).
     getInner leftValues rightValues = let
       allValues = leftValues `cross` rightValues
@@ -174,6 +192,9 @@ generateCorridors rooms = do
       min@(minA, minB) = allValues !! minIndex
       in if minA < minB then min
                         else (minB, minA)
+    updateCrossedRooms :: Room -> Room
+    updateCrossedRooms corridor@(Corridor legs crossedRooms) =
+      corridor {corridorGoesThroughRooms = S.union crossedRooms (S.fromList $ filter (roomIntersects corridor) rooms)}
 
 cross :: (a, a) -> (a, a) -> [(a,a)]
 (a1, a2) `cross` (a3, a4) = zip [a1,a2] [a3,a4]
