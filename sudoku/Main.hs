d461ea955d1365ee24c85e13d42504e6adc24cd7
korneliusz <korneliusz.caputa@siili.fi>
2016-01-15 22:22:14 +0100

    Add solving logic.

--- sudoku/Main.hs
+++ sudoku/Main.hs
@@ -1,4 +1,5 @@
 module Sudoku where
+
 import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.State
@@ -9,38 +10,51 @@ import Data.List
 -- moves :: SudokuState -> [Move] -> SudokuState
 -- moves = foldl move
 -- Monad ready
-doMove :: Move -> SudokuState ()
-doMove ((x,y),value) = update x y value
-doMoves :: [Move] -> SudokuState ()
-doMoves = mapM_ doMove
+executeMove :: Move -> SudokuState ()
+executeMove ((x,y),value) = update x y value
+
+executeMoves :: [Move] -> SudokuState ()
+executeMoves = mapM_ executeMove
+
 type StatePlus s a = StateT s Maybe a
 type SudokuGrid = [[Int]]
 type Position = (Int, Int)
 type Move = (Position, Int)
+
 type SudokuState a = StatePlus SudokuGrid a
+
 gridRange :: [Int]
 gridRange = [0 .. 8]
+
 times :: Int -> a -> [a]
 times n v = take n $ repeat v
+
 emptyRow :: [Int]
 emptyRow = times 9 0
+
 initialGrid :: SudokuGrid
 initialGrid = times 9 emptyRow
+
 getRows :: SudokuState [[Int]]
 getRows = gets id
+
 getRow :: Int -> SudokuState [Int]
 getRow index = do
     rows <- getRows
     return $ rows !! index
+
 selectRows :: [[a]] -> [Int] -> [[a]]
 selectRows values range = map (values !!) range
+
 selectColumns :: [[a]] -> [Int] -> [[a]]
 selectColumns values range = map (selectColumn values) range
     where selectColumn :: [[a]] -> Int -> [a]
           selectColumn values n = map (flip (!!) n) values
+
 getColumns :: SudokuState [[Int]]
 getColumns = gets $
     flip selectColumns gridRange
+
 getColumn :: Int -> SudokuState [Int]
 getColumn index = do
     columns <- getColumns
@@ -138,6 +152,10 @@ getEmptyPositions = do
             (\(_,v) ->
                   v == 0) $
         allCellValues
+
+initialize  :: [String] -> SudokuState ()
+initialize = executeMoves . map makeMove
+
 containingSubgrid :: Position -> Int
 containingSubgrid (x,y) = (x `div` 3) +
     (3 *
@@ -165,11 +183,72 @@ atom _ = False
 
 -- Forced moves are the ones without possible alternatives.
 getForcedMoves :: SudokuState [Move]
+                  
 getForcedMoves = do
   possibleMoves <- getPossibleMoves
   let forcedMoves = filter (atom) possibleMoves
   return $ [(pos, v)| (pos, (v:_)) <- forcedMoves]
 
-main :: IO ()
-main = do
-    print "Hi"
+executeForcedMoves :: SudokuState ()
+executeForcedMoves = do
+  forcedMoves <- getForcedMoves
+  if null forcedMoves
+     then return ()
+     else executeMoves forcedMoves >> executeForcedMoves
+
+getNextMove :: SudokuState [Move]
+getNextMove = do
+  possibleMoves <- getPossibleMoves
+  let blocked = filter (null . snd) possibleMoves
+  if null possibleMoves
+     then return []
+     else if null blocked
+             then return $ fewestOptions possibleMoves
+             else mzero
+
+-- fewestOptions :: [(Position, [a])] -> (Position, [a])
+fewestOptions es =
+  let (e:_) = sortBy (\(_,cs1) (_,cs2) -> compare (length cs1) (length cs2)) es
+      (pos, cs) = e
+   in [(pos,c) | c <- cs]
+
+solve = do
+  executeForcedMoves
+  valid <- checkValid
+  if valid
+     then do
+        nextMove <- getNextMove
+        if null nextMove
+           then return ()
+           else msum . map (\move -> executeMove move >> solve) $ nextMove
+        else mzero
+
+checkValid :: SudokuState Bool
+checkValid = do
+  rows <- getRows
+  columns <- getColumns
+  subgrids <- getSubgrids
+  let valid = foldr (\b x -> x && (allValid b)) True [rows, columns, subgrids] 
+  return True
+
+allValid :: [[Int]] -> Bool
+allValid = foldr (\x b -> b && (noDups x)) True
+
+noDups :: [Int] -> Bool
+noDups [] = True
+noDups (x:xs) = (if x == 0 then True else not (x `elem` xs)) && noDups xs
+
+prettyPrint :: [[Int]] -> IO ()
+prettyPrint rows = mapM_ prettyPrintRow rows >> putStr "\n"
+
+prettyPrintRow :: [Int] -> IO ()
+prettyPrintRow row = mapM_ prettyPrintPosition row >> putStr "\n"
+
+prettyPrintPosition :: Int -> IO ()
+prettyPrintPosition 0 = putStr "."
+prettyPrintPosition p = putStr $ show p
+
+play :: [String] -> IO ()
+play moves = case (execStateT (initialize moves >> solve) initialGrid) of
+  Just result -> prettyPrint result
+  Nothing -> putStrLn "Failed."
